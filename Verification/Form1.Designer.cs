fbeab18689f68cc2d9ee8c8867d64f7a9f462f58
SkyblueMr <skyblue_mr@outlook.com>
2015-01-30 15:32:53 +0800

    20150130

---
+++ Verification/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Verification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(92, 53);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Verification.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(92, 109);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Verification/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Verification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bit = new Bitmap(Properties.Resources._1);
+
+            for (int i = 0; i < bit.Height; ++i)
+            {
+                for (int j = 0; j < bit.Width; ++j)
+                {
+                    int pix = GetGrayNumColor(bit.GetPixel(j, i));
+                    bit.SetPixel(j, i, Color.FromArgb(255-pix, 255-pix, 255-pix));
+                }
+            }
+
+            //pictureBox2.Image = bit;
+            pictureBox3.Image = ImageTwoValue(bit);
+            pictureBox2.Image = RemoveEdge(bit);
+        }
+
+        /// <summary>
+        /// 根据RGB,计算灰度值
+        /// </summary>
+        /// <param name="posClr"></param>
+        /// <returns></returns>
+        private int GetGrayNumColor(Color posClr)
+        {
+            return (posClr.R * 19595 + posClr.G * 38469 + posClr.B * 7472) >> 16;
+        }
+
+        /// <summary>
+        /// 颜色二值化
+        /// </summary>
+        /// <param name="bit"></param>
+        /// <returns></returns>
+        private Bitmap ImageTwoValue(Bitmap bit)
+        {
+            int bgGrayValue = 128; //灰度背景分界值
+            int CharCount = 4;  //有效字符
+            int posX1 = bit.Width, posY1 = bit.Height;
+            int posX2 = 0, posY2 = 0;
+            for (int i = 0; i < bit.Height; ++i)
+            {
+                for (int j = 0; j < bit.Width; ++j)
+                {
+                    int pixValue = bit.GetPixel(j, i).R;
+                    if (pixValue < bgGrayValue)
+                    {
+                        if (posX1 > j) posX1 = j;
+                        if (posY1 > i) posY1 = i;
+                        if (posX2 < j) posX2 = j;
+                        if (posY2 < i) posY2 = i;
+                    }
+                }
+            }
+
+            int Span = CharCount - (posX2 - posX1 + 1) % CharCount;//确保能整除
+
+            //可整除的差额数
+            if (Span < CharCount)
+            {
+                int leftSpan = Span / 2;
+
+                //分配到左边的空列，如span为单数，则右边比左边大1
+                if (posX1 > leftSpan)
+                {
+                    posX1 = posX1 - leftSpan;
+                }
+
+                if (posX2 + Span - leftSpan < bit.Width)
+                {
+                    posX2 = posX2 - Span + leftSpan;
+                }
+            }
+
+            Rectangle rect = new Rectangle(posX1, posY1, posX2 - posX1 + 1, posY2 - posY1 + 1);
+            bit = bit.Clone(rect, bit.PixelFormat);
+
+            return bit;
+        }
+
+        //private Bitmap[] GetSplitPics(Bitmap bit, int RowNum, int ColNum)
+        //{
+        //    if (RowNum == 0 || ColNum == 0)
+        //        return null;
+
+
+        //}
+
+        /// <summary>
+        /// 去边
+        /// </summary>
+        /// <param name="bit"></param>
+        /// <returns></returns>
+        private Bitmap RemoveEdge(Bitmap bit)
+        {
+            int bgGrayValue = 128;//灰度背景分界值
+            for (int i = 0; i < bit.Height; ++i)
+            {
+                for (int j = 1; j < bit.Width; ++j)
+                {
+                    int pixValue = bit.GetPixel(j, i).R;
+                    if (pixValue > bgGrayValue && bit.GetPixel(j-1, i).R > bgGrayValue)
+                    {
+                        bit.SetPixel(j, i, Color.FromArgb(255, 255, 255));
+                    }
+                }
+            }
+
+            return bit;
+        }
+
+    }
+}
+
